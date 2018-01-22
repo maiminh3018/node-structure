92759bdd2998b89048ed59151d253d58132384b9
mai minh <maiminh196@gmail.com>
2018-01-22 09:10:51 +0700

    fix export setting

--- config/production/app.js
+++ config/production/app.js
@@ -1,5 +1,11 @@
 exports.app = {
-    app_url: 'https://',
+    app_url: 'https://timcongsu.vn',
     timezone: 'Asia/Ho_Chi_Minh',
     locale: 'vi',
+
+    //must not change this
+    root_path: process.cwd(),
+    lib_path: process.cwd() + '/lib',
+    lib_db_path: process.cwd() + '/lib/db',
+
 };
